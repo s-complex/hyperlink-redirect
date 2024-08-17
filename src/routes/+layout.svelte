7924a70e8b1745c054c3f9cf3efb313ec66bfaa9
gxres042 <i@gxres.net>
2024-08-17 13:18:21 +0800

    Using Noto Sans

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,14 @@
 	<title>Hyperlink Redirect</title>
 	<meta name="description" content="Hyperlink redirect site by Sliver Complex" />
 	<link rel="icon" href="https://library.gxres.net/images/icons/favicon.webp" />
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC&display=swap"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC&family=Noto+Sans:ital@0;1&display=swap"
+	/>
 </svelte:head>
 
 <div class="h-svh mx-auto max-w-2xl flex items-center justify-center">
@@ -21,6 +29,7 @@
 <style>
 	:global(body) {
 		--apply: 'm-0 bg-gradient-to-r from-blue-600 from-5% via-sky-600 via-30% to-cyan-600 to-80% text-black dark:text-white';
+		font-family: 'Noto Sans', 'Noto Sans SC';
 	}
 	:global(a) {
 		--apply: 'text-sky-700 dark:text-sky-500 no-underline hover:underline';
